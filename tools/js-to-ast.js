f3c1f9ce20d9fc0ecd023f77e62a529143846440
Michael <mgirvan838@gmail.com>
2022-02-23 14:23:21 +0000

    Improved functionality of tool.

--- tools/js-to-ast.js
+++ tools/js-to-ast.js
@@ -1,5 +1,17 @@
-import * as fs from "fs"
-var contents = fs.readFileSync(process.argv[process.argv.length - 1]).toString();
+import * as fs from 'fs'
+import * as acorn from 'acorn'
 
-import * as acorn from "acorn"
-console.log(JSON.stringify(acorn.parse(contents, {ecmaVersion: 2020})));
+if (process.argv.length == 2) {
+    console.log('Please provide a file for converstion to AST.')
+}
+else if (process.argv.length == 3) {
+    console.log('Pleas provide a path for the output file.')
+}
+else if (process.argv.length >= 4) {
+    const sourceCode = fs.readFileSync(process.argv[2])
+    let ast = JSON.stringify(acorn.parse(sourceCode, {ecmaVersion: 2020}))
+    ast = ast.replace(/,/gmi, ',\n')
+    ast += '\n'
+    fs.writeFileSync(process.argv[3], ast)
+    console.log('AST successfully written to ' + process.argv[3])
+}
