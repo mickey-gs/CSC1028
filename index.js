8ba7ffea61a6044d61f4307f75801f601d2d4969
Michael <mgirvan838@gmail.com>
2022-02-27 11:24:17 +0000

    Switched to module export statements

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { PyTranspiler } from "./src/py-transpiler/py-transpiler.js";
 import { RubyTranspiler } from "./src/ruby-transpiler/ruby-transpiler.js";
 import * as acorn from "acorn";
 
-exports.toRuby = function(code) {
+export function toRuby(code) {
   let ast = acorn.parse(code)
   const transpiler = new RubyTranspiler
 
   return transpiler.parse(ast)
 }
 
-exports.toPython = function(code) {
+export function toPython(code) {
   let ast = acorn.parse(code)
   const transpiler = new PyTranspiler
 
