3447b6547e4c15e0db136117f9d4591743ab0f1d
Michael <mgirvan838@gmail.com>
2022-03-23 15:46:08 +0000

    Migrated from using acorn for parsing to babel

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import { JSTranspiler } from "./src/js-transpiler/js-transpiler.js";
 import { PyTranspiler } from "./src/py-transpiler/py-transpiler.js";
 import { RubyTranspiler } from "./src/ruby-transpiler/ruby-transpiler.js";
-import * as acorn from "acorn";
+import * as babel from "@babel/parser";
 
 export function toRuby(code) {
-  let ast = acorn.parse(code, {ecmaVersion: 'latest', sourceType: 'module'})
+  let ast = babel.parse(code, {ecmaVersion: 'latest', sourceType: 'module'})
   const transpiler = new RubyTranspiler
 
   return transpiler.parse(ast)
 }
 
 export function toPython(code) {
-  let ast = acorn.parse(code, {ecmaVersion: 'latest', sourceType: 'module'})
+  let ast = babel.parse(code, {ecmaVersion: 'latest', sourceType: 'module'})
   const transpiler = new PyTranspiler
 
   return transpiler.parse(ast)
