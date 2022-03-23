1c5c5db87c61f596aa65311c2dfaaf27e2d04885
Michael <mgirvan838@gmail.com>
2022-03-23 16:36:14 +0000

    acorn swapped for babel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { PyTranspiler } from "./src/py-transpiler/py-transpiler.js";
 import { RubyTranspiler } from "./src/ruby-transpiler/ruby-transpiler.js";
-import * as babel from "@babel/parser";
+import * as babel from "@babel/parser"
 
 export function toRuby(code) {
   let ast = babel.parse(code, {ecmaVersion: 'latest', sourceType: 'module'})
